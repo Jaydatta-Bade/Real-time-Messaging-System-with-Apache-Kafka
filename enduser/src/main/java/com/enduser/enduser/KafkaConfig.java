4c8b7fc93f037bcae4d53a66771e3098529a0466
Jaydatta Bade <jaydattabade11@gmail.com>
2023-11-23 02:03:38 +0530

    completed

---
+++ enduser/src/main/java/com/enduser/enduser/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.enduser.enduser;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.sql.SQLOutput;
+
+@Configuration
+public class KafkaConfig {
+
+    @KafkaListener(topics = AppConstants.LOCATION_UPDATE_TOPIC, groupId = AppConstants.GROUP_ID)
+    public void updatedLocation(String value){
+
+        System.out.println(value);
+
+    }
+}
